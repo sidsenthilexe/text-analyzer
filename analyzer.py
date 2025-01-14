ec5db33f34976c53478c8caf9f0733677bc0bebe
Sid Senthil <sidsenthil2010@gmail.com>
2025-01-13 21:24:41 -0800

    update analyzer.py create a basic gui

--- analyzer.py
+++ analyzer.py
@@ -1,3 +1,25 @@
 # text analyzer in python
 
 from readability import Readability
+import sys
+from PySide6.QtCore import (QSize, Qt)
+from PySide6.QtWidgets import (QApplication, QWidget, QMainWindow, QPlainTextEdit, QComboBox, QLineEdit, QLabel, QHBoxLayout, QVBoxLayout, QPushButton, QCheckBox)
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Text Analyzer")
+        layout = QHBoxLayout()
+
+        self.input_text = QPlainTextEdit()
+        layout.addWidget(self.input_text)
+
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+    
+application = QApplication(sys.argv)
+window = Window()
+window.show()
+application.exec()
