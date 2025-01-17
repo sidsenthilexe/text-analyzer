04e187a2f6e7349d930692dd63d8d8b6b9cb14e1
Sid Senthil <sidsenthil2010@gmail.com>
2025-01-16 16:07:50 -0800

    update README.md and analyzer.py

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # text-analyzer
 A python app that analyzes inputted text with various metrics.
+
+# Requisites
+Readability 'pip install py-readability-metrics', 'python -m nltk.downlader punkt'
+
--- analyzer.py
+++ analyzer.py
@@ -5,24 +5,36 @@ import sys
 from PySide6.QtCore import (QSize, Qt)
 from PySide6.QtWidgets import (QApplication, QWidget, QMainWindow, QPlainTextEdit, QComboBox, QLineEdit, QLabel, QHBoxLayout, QVBoxLayout, QPushButton, QCheckBox)
 
+def read_text(text, metric_type):
+    pass
+
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("Text Analyzer")
-        layout1 = QVBoxLayout()
-        layout2 = QHBoxLayout()
+        column1 = QVBoxLayout()
+        column2 = QVBoxLayout()
+        row1 = QHBoxLayout()
+        row1.addLayout(column1)
+        row1.addLayout(column2)
 
         self.input_text = QPlainTextEdit()
-        layout1.addWidget(self.input_text)
-        layout2.addLayout(layout1)
+        column1.addWidget(self.input_text)
+        
 
         self.go_button = QPushButton("Analyze")
-        layout2.addWidget(self.go_button)
+        column2.addWidget(self.go_button)
+
+        self.input_metric = QComboBox()
+        self.input_metric.addItems(["Flesch-Kinaid Grade Level", "Flesch Reading Ease", "Dale Chall Readability", "Automated Readability Index", "Coleman Liau Index", "Gunning Fog Index", "SMOG Index", "SPACHE Readability Formula", "Linsear Write"])
+        column2.addWidget(self.input_metric)
 
 
+
+        
         widget = QWidget()
-        widget.setLayout(layout2)
+        widget.setLayout(row1)
         self.setCentralWidget(widget)
     
 application = QApplication(sys.argv)
