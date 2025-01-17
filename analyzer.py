a8ff908b896cd366f78ae9ee83c7a6cd63d1cfe2
Sid Senthil <sidsenthil2010@gmail.com>
2025-01-16 17:16:11 -0800

    update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -6,7 +6,47 @@ from PySide6.QtCore import (QSize, Qt)
 from PySide6.QtWidgets import (QApplication, QWidget, QMainWindow, QPlainTextEdit, QComboBox, QLineEdit, QLabel, QHBoxLayout, QVBoxLayout, QPushButton, QCheckBox)
 
 def read_text(text, metric_type):
-    pass
+    
+    readability_analyze = Readability(text)
+
+    if metric_type == "Flesch-Kinaid Grade Level":
+        fk = readability_analyze.flesch_kincaid()
+        score = fk.score
+        grade_level = fk.grade_level
+    elif metric_type == "Flesch Reading Ease":
+        f = readability_analyze.flesch()
+        score = f.score
+        ease = f.ease
+        grade_level = f.grade_levels
+    elif metric_type == "Dale Chall Readability":
+        dc = readability_analyze.dale_chall()
+        score = dc.score
+        grade_level = dc.grade_levels
+    elif metric_type == "Automated Readability Index":
+        ari = readability_analyze.ari()
+        score = ari.score
+        ages = ari.ages
+        grade_level = ari.grade_levels
+    elif metric_type == "Coleman Liau Index":
+        cl = readability_analyze.coleman_liau()
+        score = cl.score
+        grade_level = cl.grade_level
+    elif metric_type == "Gunning Fog Index":
+        gf = readability_analyze.gunning_fog()
+        score = gf.score
+        grade_level = gf.grade_level
+    elif metric_type == "SMOG Index":
+        smog = readability_analyze.smog(all_sentences=True)
+        score = smog.score
+        grade_level = smog.grade_level
+    elif metric_type == "SPACHE Readability Formula":
+        spache = readability_analyze.spache()
+        score = spache.score
+        grade_level = spache.grade_level
+    elif metric_type == "Linsear Write":
+        lw = readability_analyze.linsear_write()
+        score = lw.score
+        grade_level = lw.grade_level
 
 class Window(QMainWindow):
     def __init__(self):
